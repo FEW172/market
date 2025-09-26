0cf3bf5baf1b65ee7c5c495810e3dc2dcaacdf45
FEW172 <few_172@mail.ru>
2025-09-26 19:55:51 +0300

    Fix фильтра availability

--- src/components/container/Goods.jsx
+++ src/components/container/Goods.jsx
@@ -1,12 +1,15 @@
 import GoodsItem from "./GoodsItem"
 
 export default function Goods({ goodsData }) {
-      const goods = goodsData
-             .map(goodsItem => <GoodsItem key={goodsItem.id} goodsData={goodsItem} />)
+    const goods = goodsData
+        .filter(goodsItem => goodsItem.availability)
+        .map(goodsItem => <GoodsItem key={goodsItem.id} goodsData={goodsItem} />)
 
-        return (
+    return (
+        <>
             <div>
                 {goods}
             </div>
-        )
+        </>
+    )
 }
