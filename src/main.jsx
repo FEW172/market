29a12c15ed7c4c0f6bd86810dc79699c2c223b98
FEW172 <few_172@mail.ru>
2025-10-10 14:52:05 +0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from "./components/plugins/router"
+import { router } from "./plugins/router"
 
 createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
--- src/plugins/router.jsx
+++ src/plugins/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../../App";
-import ErrorPage from "../../pages/error-page";
-import MainPage from "../../pages/main-page";
-import ProductShowcase from "../../pages/product-showcase/ProductShowcase";
-import ShoppingCart from "../../pages/shopping-cart/ShoppingCart";
-import ProductPage from "../../pages/product-showcase/ProductPage";
-import { loadProductById, loadProducts, loadProductsTop5Rating, loadProductsByCategory } from "../../api/products/loaders"
+import App from "../App";
+import ErrorPage from "../pages/error-page";
+import MainPage from "../pages/main-page";
+import ProductShowcase from "../pages/product-showcase/ProductShowcase";
+import ShoppingCart from "../pages/shopping-cart/ShoppingCart";
+import ProductPage from "../pages/product-showcase/ProductPage";
+import { loadProductById, loadProducts, loadProductsTop5Rating, loadProductsByCategory } from "../api/products/loaders"
 
 const routes = [
     {
